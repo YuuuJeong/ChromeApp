b18555fc4842609e69b6bf9cd36d57c86bd61640
Yu Jeong Ho <dbwjdgh03@ajou.ac.kr>
2021-07-29 02:27:30 +0900

    ChromeApp Login page
    Forget committing each part of the lecture

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -3,19 +3,24 @@ const loginInput = loginForm.querySelector("#login-form input");
 const link = document.querySelector("a");
 const h1 = document.querySelector("h1");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
   h1.innerText = `Hello ${username} to meet you  `;
   h1.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-
-function handleLinkClick(event) {
-  event.preventDefault();
+function paintGreetings(username) {
+  h1.innerText = `Hello ${username} to meet you  `;
+  h1.classList.remove(HIDDEN_CLASSNAME);
 }
-
-link.addEventListener("click", handleLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
