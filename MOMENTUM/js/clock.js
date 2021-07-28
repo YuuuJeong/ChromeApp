2daf85649fb272874fd50a68134eeaa3deba00bb
Yu Jeong Ho <dbwjdgh03@ajou.ac.kr>
2021-07-29 03:31:06 +0900

    Add padStart function
    padStart 함수는 인자를 두개 받는데 첫번째 인자로 오는 것은 몇글자를 맞출지이며, 부족한 자릿수(앞쪽에 채워짐)를 뒤에 인자로 채움 ex) padStart(2, "0")이면 "1"을 "01"로 채움

--- MOMENTUM/js/clock.js
+++ MOMENTUM/js/clock.js
@@ -2,11 +2,10 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  if (date.getSeconds() < 10) {
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:0${date.getSeconds()}`;
-  } else {
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  }
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
