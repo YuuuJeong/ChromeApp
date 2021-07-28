d19715705d968ef70bf83128595898aa059f60da
Yu Jeong Ho <dbwjdgh03@ajou.ac.kr>
2021-07-29 04:00:17 +0900

    make Random background image & Add background.js

---
+++ MOMENTUM/js/background.js
@@ -0,0 +1,8 @@
+const images = ["img/0.jpeg", "img/1.jpeg", "img/2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+
+bgImage.src = `${chosenImage}`;
+
+document.body.appendChild(bgImage);
