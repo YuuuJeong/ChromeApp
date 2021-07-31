9c47787fc757c8efccc7cb715b4a6a8fc84f09e4
Yu Jeong Ho <dbwjdgh03@ajou.ac.kr>
2021-07-31 14:09:28 +0900

    implement todo, weather js file

---
+++ MOMENTUM/js/todo.js
@@ -0,0 +1,52 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+function deleteToDo(event) {
+  const deleteli = event.target.parentElement;
+  deleteli.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(deleteli.id));
+  saveToDos();
+}
+function paintToDo(newTodoObj) {
+  const li = document.createElement("li");
+  li.id = newTodoObj.id;
+  const span = document.createElement("span");
+  span.innerText = newTodoObj.text;
+  const btn = document.createElement("button");
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(btn);
+  toDoList.appendChild(li);
+}
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  console.log(toDoInput.value);
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello() {}
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
